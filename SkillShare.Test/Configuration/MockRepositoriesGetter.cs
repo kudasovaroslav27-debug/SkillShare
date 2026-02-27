93979bd0805cf00f9cd258f8d17b2eca04049d64
kudashov Yaroslav <kudasovaroslav27@gmail.com>
2026-02-27 16:26:01 +0300

    Добавил Unit тесты

---
+++ SkillShare.Test/Configuration/MockRepositoriesGetter.cs
@@ -0,0 +1,70 @@
+﻿using MockQueryable.Moq;
+using Moq;
+using SkillShare.Domain.Entities;
+using SkillShare.Domain.Interfaces.Repositories;
+
+namespace SkillShare.Test.Configuration;
+
+public static class MockRepositoriesGetter
+{
+    public static Mock<IBaseRepository<Course>> GetMockCourseRepository()
+    {
+        var mock = new Mock<IBaseRepository<Course>>();
+
+        var courses  = GetCourses().BuildMockDbSet();
+        mock.Setup (u  => u.GetAll()).Returns(valueFunction: () => courses.Object);
+        return mock;
+    }
+
+    public static Mock<IBaseRepository<User>> GetMockUserRepository()
+    {
+        var mock = new Mock<IBaseRepository<User>>();
+
+        var users  = GetUsers().BuildMockDbSet();
+        mock.Setup( u  => u.GetAll()).Returns(valueFunction: () => users.Object);
+        return mock;
+    }
+
+    public static IQueryable<Course> GetCourses()
+    {
+        return new List<Course>() {
+        new Course() {
+            Id = 1,
+            Title = "#1",
+            Description = "Информация пока нет",
+            CreatedAt = DateTime.Now.AddDays(-2),
+            UpdateAt = DateTime.Now.AddDays(-2)
+        },
+        new Course() {
+            Id = 2,
+            Title = "#2",
+            Description = "Информация пока нет",
+            CreatedAt = DateTime.Now.AddDays(-2),
+            UpdateAt = DateTime.Now.AddDays(-2)
+        }
+    }.AsQueryable(); 
+}
+
+    public static IQueryable<User> GetUsers()
+    {
+        return new List<User>() {
+        new User() {
+            Id = 1,
+            Login = "ITSkillShare",
+            Password = "dasdKAJSkdqwej4ej#",
+            CreatedAt = DateTime.Now.AddDays(-2),
+            UpdateAt = DateTime.Now.AddDays(-2)
+        },
+        new User() {
+            Id = 2,
+            Login = "Basne",
+            Password = "daskdak@K#K@",
+            CreatedAt = DateTime.Now.AddDays(-2),
+            UpdateAt = DateTime.Now.AddDays(-2)
+        }
+    }.AsQueryable();
+
+    }
+}
+
+
---
+++ SkillShare.Test/CourseServiceTest.cs
@@ -0,0 +1,126 @@
+﻿using Mapster;
+using MapsterMapper;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Options;
+using Moq;
+using SkillShare.Application.Services;
+using SkillShare.Domain.Dto.CourseDto;
+using SkillShare.Domain.Entities;
+using SkillShare.Domain.Interfaces.Repositories;
+using SkillShare.Domain.Interfaces.Validations;
+using SkillShare.Domain.Result;
+using SkillShare.Domain.Settings;
+using SkillShare.Producer.Interfaces;
+using SkillShare.Test.Configuration;
+using Xunit;
+
+namespace SkillShare.Test;
+
+
+public class CourseServiceTests
+{
+    private IMapper GetMapper()
+    {
+        var config = new TypeAdapterConfig();
+
+        var mockServiceProvider = new Mock<IServiceProvider>();
+
+        return new ServiceMapper(mockServiceProvider.Object, config);
+    }
+
+
+    [Fact]
+    public async Task GetCourse_ShouldBe_NotNull()
+    {
+        // Arrange
+        var mockRepository = MockRepositoriesGetter.GetMockCourseRepository();
+        var mockDistributedCache = new Mock<IDistributedCache>();
+        var mapper = GetMapper();
+
+        var reportService = new CourseService(mockRepository.Object, null, mapper, null,
+         null, null, null, mockDistributedCache.Object);
+
+        // Act
+        var result = await reportService.GetByIdAsync(1);
+
+        // Assert
+        Assert.NotNull(result);
+    }
+
+    [Fact] 
+    public async Task CreateCourse_ShouldBe_Return_NewCourse()
+    {
+        // Arrange
+        var mockCourseRepository = MockRepositoriesGetter.GetMockCourseRepository();
+        var mockUserRepository = MockRepositoriesGetter.GetMockUserRepository();
+        var mockDistributedCache = new Mock<IDistributedCache>();
+        var mapper = GetMapper();
+
+        var mockMessageProducer = new Mock<IMessageProducer>();
+        var mockValidator = new Mock<ICourseValidator>();
+        var mockOptions = new Mock<IOptions<RabbitMqSettings>>();
+        mockOptions.Setup(o => o.Value).Returns(new RabbitMqSettings { RoutingKey = "test", ExchangeName = "test" });
+        mockValidator.Setup(v => v.ValidatorCreate(It.IsAny<Course>(), It.IsAny<User>())).Returns(BaseResult.Success());
+
+        var user = MockRepositoriesGetter.GetUsers().FirstOrDefault();
+        var createCourseDto = new CreateCourseDto(
+            Title: "Test Course",
+            Description: "Test Description",
+            Price: 100,
+            ParentId: null
+        );
+
+        // Act 
+        var courseService = new CourseService(
+            mockCourseRepository.Object,
+            null, 
+            mapper,                     
+            null,   
+            mockUserRepository.Object,                    
+            mockMessageProducer.Object,
+            mockOptions.Object,       
+            mockDistributedCache.Object
+        );
+
+        var result = await courseService.CreateAsync(user.Id, createCourseDto);
+
+        // Assert
+        Assert.True(result.IsSuccess);
+        Assert.NotNull(result.Data);
+    }
+
+    [Fact]
+    public async Task DeleteCourse_ShouldBe_Return_TrueSuccess()
+    {
+        // Arrange
+        var mockCourseRepository = MockRepositoriesGetter.GetMockCourseRepository();
+        var mapper = GetMapper();
+        var course = MockRepositoriesGetter.GetCourses().FirstOrDefault();
+
+        // Act
+        var courseService = new CourseService(mockCourseRepository.Object, null, mapper, null,
+        null, null, null, null);
+        var result = await courseService.DeleteAsync(course.Id);
+
+        // Assert
+        Assert.True(result.IsSuccess);
+    }
+
+    [Fact]
+    public async Task UpdateReport_ShouldBe_Return_NewData_For_Report()
+    {
+        // Arrange
+        var mockCourseRepository = MockRepositoriesGetter.GetMockCourseRepository();
+        var mapper = GetMapper();
+        var course = MockRepositoriesGetter.GetCourses().FirstOrDefault();
+        var updateCourseDto = new UpdateCourseDto(course.Id, "New name", "New description", 100);
+
+        // Act
+        var courseService = new CourseService(mockCourseRepository.Object, null, mapper, null,
+        null, null, null, null);
+        var result = await courseService.UpdateAsync(updateCourseDto);
+
+        // Assert
+        Assert.True(result.IsSuccess);
+    }
+}
